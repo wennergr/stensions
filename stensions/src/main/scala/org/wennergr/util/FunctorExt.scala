edfe22fd9e0c36c5832c0a85a5b5b02b3580f77e
tobias <tobias@loyal3.com>
2015-02-20 13:50:36 -0800

    Improved documentation for functions

--- stensions/src/main/scala/org/wennergr/util/FunctorExt.scala
+++ stensions/src/main/scala/org/wennergr/util/FunctorExt.scala
@@ -6,9 +6,28 @@ object FunctorExt {
     def map[A, B](fb: M[A])(f: A => B): M[B]
   }
 
+  /**
+   * These functions will be added into a class that
+   * has an implicit conversion to the Functor trait. (Pimp my library pattern)
+   *
+   * @see org.wennergr.util.OptionExt
+   */
   class FunctorOps[F[_], Z](val self: F[Z])(implicit val F: Functor[F])  {
+
+    /**
+     * Lift out the Functor F[Z] from the map operation
+     */
     def fLift[B](f: Z => B): F[Z] => F[B] = F.map(_)(f)
+
+    /**
+     * Return a pair F[(Z,Z)] by duplicating the value of Z
+     * @return
+     */
     def fPair(): F[(Z,Z)] = F.map(self)(x => (x,x))
+
+    /**
+     * Transforms F[Z] to F[B] no matter what the value of F[Z] is
+     */
     def fAs[B](b: B) = F.map(self)(_ => b)
   }
 
--- stensions/src/main/scala/org/wennergr/util/MonadExt.scala
+++ stensions/src/main/scala/org/wennergr/util/MonadExt.scala
@@ -8,7 +8,23 @@ object MonadExt {
     def flatMap[A, B](fa: M[A])(f: A => M[B]): M[B]
   }
 
+  /**
+   * These functions will be added into a class that
+   * has an implicit conversion to the Monad trait. (Pimp my library pattern)
+   *
+   * @see org.wennergr.util.OptionExt
+   */
   final class MonadOps[M[_], Z](override val self: M[Z])(implicit val M: Monad[M]) extends FunctorOps[M, Z](self) {
+
+    /**
+     * Runs function f(a,b) on Monad[A] and Monad[B]
+     *
+     * Similar to for comprehension
+     * for {
+     *   a <- fa
+     *   b <- fb
+     * } yield f(a,b)
+     */
     def map2[A, B](fb: M[A])(f: (Z, A) => B): M[B] = M.flatMap(self)(a => M.map(fb)(b => f(a,b)))
   }
 }
